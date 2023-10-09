ba7f2338dafdf68e8153a39d42d49ba5dd818558
Daniel-Melo924 <dantoniomelo@unicesar.edu.co>
2023-10-09 16:57:19 -0500

    Primera Interfaz

---
+++ GUIPrinc/GestCitas.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUIPrinc
+{
+    public partial class GestCitas : Form
+    {
+        public GestCitas()
+        {
+            InitializeComponent();
+        }
+
+        private void panel5_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnVolver_Click(object sender, EventArgs e)
+        {
+            Form btVolver = new Principal();
+            btVolver.Show();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
