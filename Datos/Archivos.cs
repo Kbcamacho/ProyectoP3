a86c38f450a96845b95f35b8826700933c862bb3
Kbcamacho <kbcamacho@unicesar.edu.co>
2023-10-14 12:25:48 -0500

    Archivos

---
+++ Datos/Archivos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Datos
+{
+    public class Archivos
+    {
+        protected String Filename;
+        public Archivos(string filename)
+        {
+            this.Filename = filename;
+        }
+
+        public String Guardar(String Datos)
+        {
+            try
+            {
+                StreamWriter writer = new StreamWriter(Filename, true);
+                writer.WriteLine(Datos);
+                writer.Close();
+                return "Datos guardados correctamente";
+            }
+            catch (Exception e)
+            {
+                return "Error al guardar los datos: ";
+            }
+            
+
+        } 
+    }
+}
---
+++ Datos/CitaRepository.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using HospitalAPP;
+
+namespace Datos
+{
+    public class CitaRepository : Archivos
+    {
+        public CitaRepository(string FileName) : base(FileName)
+        {
+        }
+
+        public List<Cita> ConsultaCitas()
+        {
+            try
+            {
+                List<Cita> listaCitas = new List<Cita>();
+
+                StreamReader reader = new StreamReader(FileName);
+                while (!reader.EndOfStream)
+                {
+                    lista.add(new Mapear(reader.ReadLine()));
+                }
+                reader.Close();
+                return lista;
+            }
+            catch (Exception a)
+            {
+                Console.WriteLine(a);
+                throw;
+            }
+            return null;
+        }
+
+        public Cita BuscarcCita(String id)
+        {
+            var lista = ConsultaCitas();
+            foreach (var item in lista)
+            {
+                if (item.IdCita == id)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+
+        private Cita Mapear(String linea)
+        {
+            var datos = linea.Split(';');
+            Cita cita = new Cita();
+            cita.IdCita = datos[0];
+            cita.IdPaciente = datos[1];
+            cita.IdMedico = datos[2];
+            cita.Fecha = datos[3];
+            cita.Hora = datos[4];
+            cita.Estado = datos[5];
+            return cita;
+        }
+    }
+}
