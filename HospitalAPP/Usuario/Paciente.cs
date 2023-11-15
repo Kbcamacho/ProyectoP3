91025b1cb7a70a0a881a4be84ae16d669400656f
Kbcamacho <kbcamacho@unicesar.edu.co>
2023-11-15 15:35:23 -0500

    DUsuarios

--- HospitalAPP/Usuario/Paciente.cs
+++ HospitalAPP/Usuario/Paciente.cs
@@ -10,16 +10,19 @@ namespace HospitalAPP
     public class Paciente : Persona
     {
         public String TipoSangre { get; set; }
-
+        public String EstadoSalud { get; set; }
+        public String MedicoAsignado { get; set; }
         public Paciente()
         {
 
         }
 
-        public Paciente(String cedula, String password, String nombre, String apellido, String telefono, DateTime fechaNacimiento, String sexo, String correoElectronico, String tipoSangre) : base(cedula, password, nombre, apellido, telefono,
+        public Paciente(String cedula, String password, String nombre, String apellido, String telefono, DateTime fechaNacimiento, String sexo, String correoElectronico, String tipoSangre, String estadoSalud, String medicoAsignado) : base(cedula, password, nombre, apellido, telefono,
           fechaNacimiento, sexo, correoElectronico)
         {
             TipoSangre = tipoSangre;
+            EstadoSalud = estadoSalud;
+            MedicoAsignado = medicoAsignado;
         }
     }
 }
