d54c79c07ed9ba0839f0576676140251ff1af79e
Kbcamacho <kbcamacho@unicesar.edu.co>
2023-11-15 13:11:57 -0500

    Conexion BD

---
+++ Datos/Repositorio/DUsuarios.cs
@@ -0,0 +1,45 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class DUsuarios
+    {
+        int IdUsuario; 
+
+        public DataTable Mostrar(string cTexto)
+        {
+            OracleDataReader Resultado;
+            DataTable Tabla = new DataTable();
+            OracleConnection SqlCon = new OracleConnection();
+            try
+            {
+                cTexto = "%" + cTexto + "%"; 
+                SqlCon = ConexionBD.GetInstancia().CrearConexion();
+                OracleCommand Comando = new OracleCommand("select * from VISTA_PACIENTE where ID_PACIENTE like '" + cTexto + "' ", SqlCon);
+                Comando.CommandType = CommandType.Text;
+                SqlCon.Open();
+                Resultado = Comando.ExecuteReader();
+                Tabla.Load(Resultado);
+                return Tabla;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+
+        }
+
+
+    }
+}
