e9f38a212f9707147295f0de1a55aeebb573310b
Daniel-Melo924 <dantoniomelo@unicesar.edu.co>
2023-11-16 11:33:10 -0500

    gggg

--- GUIPrinc/Login.Designer.cs
+++ GUIPrinc/Login.Designer.cs
@@ -31,6 +31,8 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ChbUsuario = new System.Windows.Forms.CheckBox();
+            this.ChbDoctor = new System.Windows.Forms.CheckBox();
             this.btnCrearCuentaIS = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.btnLimpiarIS = new System.Windows.Forms.Button();
@@ -40,9 +42,6 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-            this.ChbDoctor = new System.Windows.Forms.CheckBox();
-            this.ChbUsuario = new System.Windows.Forms.CheckBox();
-            this.label5 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -62,7 +61,6 @@
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.Color.SteelBlue;
-            this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.ChbUsuario);
             this.groupBox1.Controls.Add(this.ChbDoctor);
             this.groupBox1.Controls.Add(this.btnCrearCuentaIS);
@@ -77,17 +75,39 @@
             this.groupBox1.ForeColor = System.Drawing.Color.LightCyan;
             this.groupBox1.Location = new System.Drawing.Point(12, 69);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(573, 311);
+            this.groupBox1.Size = new System.Drawing.Size(505, 252);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Ingrese sus datos:";
+            // 
+            // ChbUsuario
+            // 
+            this.ChbUsuario.AutoSize = true;
+            this.ChbUsuario.Location = new System.Drawing.Point(23, 155);
+            this.ChbUsuario.Name = "ChbUsuario";
+            this.ChbUsuario.Size = new System.Drawing.Size(98, 24);
+            this.ChbUsuario.TabIndex = 10;
+            this.ChbUsuario.Text = "Usuarios";
+            this.ChbUsuario.UseVisualStyleBackColor = true;
+            this.ChbUsuario.CheckedChanged += new System.EventHandler(this.ChbUsuario_CheckedChanged);
+            // 
+            // ChbDoctor
+            // 
+            this.ChbDoctor.AutoSize = true;
+            this.ChbDoctor.Location = new System.Drawing.Point(146, 155);
+            this.ChbDoctor.Name = "ChbDoctor";
+            this.ChbDoctor.Size = new System.Drawing.Size(82, 24);
+            this.ChbDoctor.TabIndex = 3;
+            this.ChbDoctor.Text = "Doctor";
+            this.ChbDoctor.UseVisualStyleBackColor = true;
+            this.ChbDoctor.CheckedChanged += new System.EventHandler(this.ChbDoctor_CheckedChanged);
             // 
             // btnCrearCuentaIS
             // 
             this.btnCrearCuentaIS.BackColor = System.Drawing.Color.LightCyan;
             this.btnCrearCuentaIS.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCrearCuentaIS.ForeColor = System.Drawing.Color.Black;
-            this.btnCrearCuentaIS.Location = new System.Drawing.Point(238, 222);
+            this.btnCrearCuentaIS.Location = new System.Drawing.Point(269, 208);
             this.btnCrearCuentaIS.Name = "btnCrearCuentaIS";
             this.btnCrearCuentaIS.Size = new System.Drawing.Size(146, 28);
             this.btnCrearCuentaIS.TabIndex = 9;
@@ -98,7 +118,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(30, 225);
+            this.label4.Location = new System.Drawing.Point(67, 211);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(186, 20);
             this.label4.TabIndex = 8;
@@ -108,7 +128,7 @@
             // 
             this.btnLimpiarIS.BackColor = System.Drawing.Color.Wheat;
             this.btnLimpiarIS.ForeColor = System.Drawing.Color.Black;
-            this.btnLimpiarIS.Location = new System.Drawing.Point(113, 147);
+            this.btnLimpiarIS.Location = new System.Drawing.Point(269, 147);
             this.btnLimpiarIS.Name = "btnLimpiarIS";
             this.btnLimpiarIS.Size = new System.Drawing.Size(96, 38);
             this.btnLimpiarIS.TabIndex = 6;
@@ -120,7 +140,7 @@
             // 
             this.btnIngresarIS.BackColor = System.Drawing.Color.LightBlue;
             this.btnIngresarIS.ForeColor = System.Drawing.Color.Black;
-            this.btnIngresarIS.Location = new System.Drawing.Point(363, 147);
+            this.btnIngresarIS.Location = new System.Drawing.Point(392, 147);
             this.btnIngresarIS.Name = "btnIngresarIS";
             this.btnIngresarIS.Size = new System.Drawing.Size(96, 38);
             this.btnIngresarIS.TabIndex = 4;
@@ -130,24 +150,24 @@
             // 
             // txtPassPaCC
             // 
-            this.txtPassPaCC.Location = new System.Drawing.Point(113, 103);
+            this.txtPassPaCC.Location = new System.Drawing.Point(146, 103);
             this.txtPassPaCC.Name = "txtPassPaCC";
             this.txtPassPaCC.PasswordChar = '*';
-            this.txtPassPaCC.Size = new System.Drawing.Size(375, 27);
+            this.txtPassPaCC.Size = new System.Drawing.Size(342, 27);
             this.txtPassPaCC.TabIndex = 3;
             // 
             // txtUserPaCC
             // 
-            this.txtUserPaCC.Location = new System.Drawing.Point(113, 38);
+            this.txtUserPaCC.Location = new System.Drawing.Point(146, 38);
             this.txtUserPaCC.Name = "txtUserPaCC";
-            this.txtUserPaCC.Size = new System.Drawing.Size(375, 27);
+            this.txtUserPaCC.Size = new System.Drawing.Size(342, 27);
             this.txtUserPaCC.TabIndex = 2;
             this.txtUserPaCC.TextChanged += new System.EventHandler(this.txtUserPaCC_TextChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(2, 103);
+            this.label3.Location = new System.Drawing.Point(19, 103);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(100, 20);
             this.label3.TabIndex = 1;
@@ -156,7 +176,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 45);
+            this.label2.Location = new System.Drawing.Point(19, 45);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(72, 20);
             this.label2.TabIndex = 0;
@@ -173,43 +193,12 @@
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "linkLabel1";
             // 
-            // ChbDoctor
-            // 
-            this.ChbDoctor.AutoSize = true;
-            this.ChbDoctor.Location = new System.Drawing.Point(406, 256);
-            this.ChbDoctor.Name = "ChbDoctor";
-            this.ChbDoctor.Size = new System.Drawing.Size(82, 24);
-            this.ChbDoctor.TabIndex = 3;
-            this.ChbDoctor.Text = "Doctor";
-            this.ChbDoctor.UseVisualStyleBackColor = true;
-            this.ChbDoctor.CheckedChanged += new System.EventHandler(this.ChbDoctor_CheckedChanged);
-            // 
-            // ChbUsuario
-            // 
-            this.ChbUsuario.AutoSize = true;
-            this.ChbUsuario.Location = new System.Drawing.Point(286, 257);
-            this.ChbUsuario.Name = "ChbUsuario";
-            this.ChbUsuario.Size = new System.Drawing.Size(98, 24);
-            this.ChbUsuario.TabIndex = 10;
-            this.ChbUsuario.Text = "Usuarios";
-            this.ChbUsuario.UseVisualStyleBackColor = true;
-            this.ChbUsuario.CheckedChanged += new System.EventHandler(this.ChbUsuario_CheckedChanged);
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(6, 257);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(239, 20);
-            this.label5.TabIndex = 11;
-            this.label5.Text = "Seleccione el correspondiente:";
-            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SteelBlue;
-            this.ClientSize = new System.Drawing.Size(588, 392);
+            this.ClientSize = new System.Drawing.Size(530, 334);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
@@ -240,7 +229,6 @@
         private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btnCrearCuentaIS;
-        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.CheckBox ChbUsuario;
         private System.Windows.Forms.CheckBox ChbDoctor;
     }
